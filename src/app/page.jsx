1ca77c41057c8d49f51926346e7965b9a8ed7a74
BigSam55 <contactgoodlucksamuel@gmail.com>
2024-05-24 11:05:38 -0500

    firts branch commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,15 @@
+import Arrival from '@/Components/Arrival/page'
+import Feature from '@/Components/Featured/page'
+import Heropage from '@/Components/Heropage/page'
 import Navbar from '@/Components/Navbar/page'
 import React from 'react'
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
+      <Heropage/>
+      <Feature/>
+      <Arrival/>
     </div>
   )
 }
